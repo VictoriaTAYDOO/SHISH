a32ff8dd65ed1c864dc8ed092ad2c2d45859ec0a
Анастасия <vika.tay.doo@gmail.com>
2023-11-23 17:38:56 +0300

    Privet3

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-def main():
-    print('My first git program')
-    print('hehehe')
-    print('hahaha')
+import sys
+import random
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt5 import uic
+
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.do_paint = False
+
+    def initUI(self):
+        uic.loadUI('UI.ui')
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(100):
+            x = random.randint(20, 600)
+            y = random.randint(20, 600)
+            r = random.randint(10, 100)
+            qp.drawEllipse(x, y, r, r)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
