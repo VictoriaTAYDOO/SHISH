7c1e194253f9ba95d565ab46cb37b04968515d34
Анастасия <vika.tay.doo@gmail.com>
2023-11-23 17:56:11 +0300

    Privet5

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import random
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 from PyQt5 import uic
+from UI import UI_MainWindow
 
 
-
-class Example(QMainWindow):
+class Example(QMainWindow, UI_MainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
         self.do_paint = False
+        self.setupUi(self)
 
     def initUI(self):
-        uic.loadUI('UI.ui')
+        # uic.loadUI('UI.ui')
         self.pushButton.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -30,7 +31,7 @@ class Example(QMainWindow):
         self.update()
 
     def draw(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         for i in range(100):
             x = random.randint(20, 600)
             y = random.randint(20, 600)
